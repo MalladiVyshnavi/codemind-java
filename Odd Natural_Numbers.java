6b58f1985ccc0af77d483aaa5c4fc70599c28ec7
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-18 12:03:12 +0530

    import java.util.Scanner; public class Fact {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a,i;         a=sc.nextInt();         for(i=1;i<=a;i++)         {             if(i%2!=0)             {                 System.out.printf("%d ",i);             }         }     } }

---
+++ Odd Natural_Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Fact
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,i;
+        a=sc.nextInt();
+        for(i=1;i<=a;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.printf("%d ",i);
+            }
+        }
+    }
+}
