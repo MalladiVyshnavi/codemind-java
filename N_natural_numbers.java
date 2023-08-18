14d60d3fc95cf1146041324cd805fa1c89b1708f
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-18 11:38:33 +0530

    import java.util.Scanner; public class Num {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a,i;         a=sc.nextInt();         for(i=1;i<=a;i++)         {             System.out.printf("%d ",i);         }     } }

---
+++ N_natural_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Num
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,i;
+        a=sc.nextInt();
+        for(i=1;i<=a;i++)
+        {
+            System.out.printf("%d ",i);
+        }
+    }
+}
