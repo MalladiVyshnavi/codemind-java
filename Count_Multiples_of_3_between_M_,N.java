7562a7f18b66e2a62250dc2bde41d86c1c975d7a
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-08-18 11:55:10 +0530

    import java.util.Scanner; public class Fact {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a,b,i,c=0;         a=sc.nextInt();         b=sc.nextInt();         for(i=a;i<=b;i++)         {             if(i%3==0)             {                 c=c+1;             }         }         System.out.printf("%d ",c);
        }
    }

---
+++ Count_Multiples_of_3_between_M_,N.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Fact
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,i,c=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=a;i<=b;i++)
+        {
+            if(i%3==0)
+            {
+                c=c+1;
+            }
+        }
+        System.out.printf("%d ",c);
+        
+    }
+}
