1fc69ebd35ccdd0093712726218125538d48099d
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-09-07 20:03:55 +0530

    import java.util.Scanner; public class Tables {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a=sc.nextInt();         for( int i=1;i<=a;i++)         {             if(i%2!=0)             {                 System.out.printf("%d x %d = %d ",n,i,n*i);             }         }     } }

---
+++ Tables.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Tables
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        for( int i=1;i<=a;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.printf("%d x %d = %d
+",n,i,n*i);
+            }
+        }
+    }
+}
