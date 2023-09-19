aaa81a68948c13ba6f597fe63d55a8657e793be0
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-09-19 19:59:26 +0530

    import java.util.Scanner; public class Boundary {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int arr[][]=new int[n][m];         for(int i=0;i<n;i++)         {             for(int j=0;j<m;j++)             {                 arr[i][j]=sc.nextInt();             }         }         int sum=0;         for(int i=0;i<n;i++)         {             for(int j=0;j<m;j++)             {                 if(i==0 || i==n-1 || j==0 || j==m-1)                 {                     sum+=arr[i][j];                 }             }         }         System.out.println(sum);     } }

---
+++ Sum_of_all_boundary_values_in_a_matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Boundary
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int arr[][]=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 || i==n-1 || j==0 || j==m-1)
+                {
+                    sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
