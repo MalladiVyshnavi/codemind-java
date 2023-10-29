7a11d9158107116c6e9335af897d9f10ed15c16c
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-10-29 11:54:23 +0530

    import java.util.Scanner; public class Star {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=i;j++)             {                 System.out.print("*");             }             System.out.println();         }     } }

--- printing_pattern_like_a_right-angle_triangle_with_a_little_star.java
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.java
@@ -4,11 +4,10 @@ public class Star
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a,i,j;
-        a=sc.nextInt();
-        for(i=1;i<=a;i++)
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
         {
-            for(j=1;j<=i;j++)
+            for(int j=1;j<=i;j++)
             {
                 System.out.print("*");
             }
