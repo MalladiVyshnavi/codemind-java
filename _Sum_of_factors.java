ce6919951f982e77696cca9ab1f6197044955f9f
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-11-02 19:27:43 +0530

    import java.util.Scanner; public class Fact {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int sum=0;         for(int i=1;i<a;i++)         {             if(a%i==0)             {                 sum=sum+i;             }         }         System.out.printf("%d",sum);     } }

--- _Sum_of_factors.java
+++ _Sum_of_factors.java
@@ -4,13 +4,13 @@ public class Fact
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a,i,sum=0;
-        a=sc.nextInt();
-        for(i=1;i<a;i++)
+        int a=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<a;i++)
         {
             if(a%i==0)
             {
-            sum=sum+i;
+                sum=sum+i;
             }
         }
         System.out.printf("%d",sum);
