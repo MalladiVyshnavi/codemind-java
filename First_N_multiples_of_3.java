7b2138a8ed8f921784446d33ea2aa32b1369a66d
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-11-02 19:43:24 +0530

    import java.util.Scanner; public class Multiple {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             System.out.print(3*i+" ");         }     } }

--- First_N_multiples_of_3.java
+++ First_N_multiples_of_3.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
-public class Fact
+public class Multiple
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a,i;
-        a=sc.nextInt();
-        for(i=1;i<=(a*3);i++)
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
         {
-            if(i%3==0)
-            {
-                System.out.printf("%d ",i);
-            }
+            System.out.print(3*i+" ");
         }
     }
 }
