af8b9a716a3cfb3d34beb14b2aa61f926adaa2f9
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-11-02 19:34:34 +0530

    import java.util.Scanner; public class Count {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int m=sc.nextInt();         int n=sc.nextInt();         int count=0;         for(int i=m;i<=n;i++)         {           if(i%2==0 && i%3==0)           {               count++;           }         }         System.out.println(count);
        }
    }

--- Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.java
+++ Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
-public class Fact
+public class Count
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a,b,i,c=0;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        for(i=a;i<=b;i++)
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=m;i<=n;i++)
         {
-            if(i%2==0 && i%3==0)
-            {
-                c=c+1;
-            }
+          if(i%2==0 && i%3==0)
+          {
+              count++;
+          }
         }
-        System.out.printf("%d ",c);
+        System.out.println(count);
+       
     }
 }
