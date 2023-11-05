25e9ef645733f1db46c18a28c4f6ed6b0f959c8e
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2023-11-05 19:47:11 +0530

    import java.util.Scanner; public class Cars {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=(n+3)/4;         System.out.println(c);
        }
    }

---
+++ Cars.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Cars
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=(n+3)/4;
+        System.out.println(c);
+        
+    }
+}
